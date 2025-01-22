bbaf409f4c70b4c2e0bcfd91cb7bd82502ed655f
LorisMarcel <151756035+LorisMarcel@users.noreply.github.com>
2025-01-22 13:51:38 +0000

    Update CV_analysis.py

--- DinoBot/CV_analysis.py
+++ DinoBot/CV_analysis.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 
 """ Read the data """
 
 
 def read_file(file):
-    with open(file) as t:
+    with open(file, encoding='utf-16') as t:
         contents_unfiltered = t.readlines()
         contents = []
         for row in contents_unfiltered:
@@ -17,9 +18,9 @@ def read_file(file):
         current = []
         for line in contents:
             potentialAg.append(float(line.split('\t')[0]))
-            # potentialAg.append(float(line.split(',')[0]))
+            # potentialAg.append(float(line.split(',')[4]))
             current.append(float(line.split('\t')[1]))
-            # current.append(float(line.split(',')[1]))
+            # current.append(float(line.split(',')[5]))
         for value in potentialAg:
             potentialSHE.append(value+0.209)
         return potentialSHE, current
@@ -29,7 +30,7 @@ def read_file(file):
 
 
 def create_df(potential, current):
-    dict = {'Potential (V)': potential, 'Current (nA)': current}
+    dict = {'Potential': potential, 'Current': current}
     df = pd.DataFrame(dict)
     return df
 
@@ -38,8 +39,8 @@ def create_df(potential, current):
 
 
 def linechart(df, title):
-    x = df["Potential (V)"]
-    y = df["Current (nA)"]
+    x = df["Potential"]
+    y = df["Current"]
 
     plt.plot(x, y, color='black')
     plt.xlabel('Potential vs SHE (V)', size=12)
@@ -49,14 +50,20 @@ def linechart(df, title):
 
 
 def linechart_difference(df, title):
-    x = df["Potential (V)"]
-    y = df["Difference (nA)"]
+    x = df["Potential"]
+    y = df["Difference"]
 
     plt.plot(x, y, color='black')
     plt.xlabel('Potential vs SHE (V)', size=12)
     plt.ylabel('Difference in current intensity (nA)', size=12)
     plt.title(title, pad='25')
     plt.show()
+
+
+def get_data(file):
+    a, b = read_file(file)
+    data = create_df(a, b)
+    return data
 
 
 def analyse_CV(file, title):
@@ -69,9 +76,29 @@ def analyse_CV(file, title):
 def analyse_difference(file1, file2, title):
     a, b, = read_file(file1)
     c, d = read_file(file2)
-    dict = {'Potential (V)': a, 'Current1 (nA)': b, 'Current2 (nA)': d}
+    dict = {'Potential': a, 'Current1': b, 'Current2': d}
     df = pd.DataFrame(dict)
-    df["Difference (nA)"] = df['Current1 (nA)'] - df['Current2 (nA)']
+    df["Difference"] = df['Current1'] - df['Current2']
     print(df)
     linechart_difference(df, title)
+
+
+def integrate(data, a, b):
+    current = data["Current"].to_list()
+    potential = data["Potential"].to_list()
+
+    half_length = len(potential) // 2
+    redlist = potential[:half_length]
+    redlist = [round(elem, 3) for elem in redlist]
+    oxlist = potential[half_length:]
+    oxlist = [round(elem, 3) for elem in oxlist]
+
+    print(current)
+    print(len(current))
+    print(len(current[0]))
+
+    A = oxlist.index(a)
+    B = oxlist.index(b)
+    area = np.trapz(current[A:B], x=potential[A:B])
+    return area
 
